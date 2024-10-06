07e369caa5a48a31a065b2164f140ff552efd547
jadTales <117829497+Jadtales@users.noreply.github.com>
2024-10-06 12:24:48 +0200

    - Worked on facilitating the scrolling for the user during the user's creating Note cards.

---
+++ frontend-layer/app/compos/notesCreationComponents/topLayerComponents/NotesCreationButtons.tsx
@@ -0,0 +1,80 @@
+'use client'
+
+import {ReactElement, useEffect, useState} from "react";
+import Image from "next/image";
+import '../notesCreationCompoStyling.css'
+
+import OpenLockIcon from '@/public/icons/notesIcons/openLockIcon.svg'
+import LockIcon from '@/public/icons/notesIcons/lockIcon.svg'
+import ArrowUpIcon from '@/public/icons/arrow-up-line.svg'
+
+export default function NotesCreationButtons(): ReactElement {
+    const [isSharingButtonClicked, setIsSharingButtonClicked] = useState<boolean>(true);
+    const [pageHeight, setPageHeight] = useState<number>(0);
+
+    const switchSharingOption = (): void => {
+        setIsSharingButtonClicked(!isSharingButtonClicked);
+    }
+
+    useEffect(() => {
+        const getScrollHeight = (): void => {
+            setPageHeight(window.scrollY);
+        };
+
+        // Debounce scroll event handler for performance
+        const debounceScroll = (): () => void => {
+            let timer: NodeJS.Timeout;
+            return (): void => {
+                clearTimeout(timer);
+                timer = setTimeout(getScrollHeight, 100);  // Call after 100ms of no scroll event
+            };
+        };
+
+        const handleDebouncedScroll = debounceScroll();
+        window.addEventListener('scroll', handleDebouncedScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleDebouncedScroll);
+        };
+    }, []);
+
+    const handleGoBackToTop = () => {
+        window.scrollTo({top: 900, behavior: 'smooth'});
+    }
+
+    return (
+        <div className="createnotes-topLayer">
+            <div
+                className={pageHeight > window.innerHeight ? 'creationButton-layer-withNavbar' : 'creationButton-layer'}>
+                <div className="lastSaveTime">
+                    Last saved, 3 seconds ago.
+                </div>
+
+                <div className="creationButtons">
+
+                    {pageHeight > window.innerHeight && <Image src={ArrowUpIcon}
+                                                               id="arrowUpIcon"
+                                                               alt="goUp"
+                                                               style={{cursor: 'pointer'}}
+                                                               onClick={handleGoBackToTop}/>}
+                    <button className="sharingOption" onClick={switchSharingOption}>
+                        {isSharingButtonClicked ?
+                            <Image src={OpenLockIcon} alt="publicNote" width={19}/>
+                            : <Image src={LockIcon} alt="publicNote" width={19}/>}
+                        {isSharingButtonClicked ?
+                            (<span>Public</span>) : <span>Private</span>}
+                    </button>
+
+                    <button className="noteCreationButton">Create</button>
+                </div>
+            </div>
+
+            <div className="notesCreationFromExternalSources-layer">
+                <h1>Import from</h1>
+
+                <button>Import highlights from your device</button>
+                <button>Import savings from X</button>
+            </div>
+        </div>
+    )
+}
---
+++ frontend-layer/app/createnotes/page.tsx
@@ -0,0 +1,103 @@
+'use client'
+
+import { ChangeEvent, ReactElement, useState } from "react";
+import './createNotesPageStyling.css';
+
+import NotesCreationButtons from "@/app/compos/notesCreationComponents/topLayerComponents/NotesCreationButtons";
+import NotesDescriptionAndTitleComponent from "@/app/compos/notesCreationComponents/topLayerComponents/NotesDescriptionAndTitleComponent";
+import NoteCard from "@/app/compos/notesCreationComponents/topLayerComponents/NoteCard";
+import GoBackToComponent from "@/app/compos/goBackTo-component/GoBackTo-Component";
+import SubModalComponent from "@/app/compos/subscriptionModal/SubModalComponent";
+
+interface NoteCardData {
+    cardKey: number;
+    cardTitle: string;
+    cardDescription: string;
+}
+
+export default function Notes(): ReactElement {
+    const [noteCards, setNoteCards] = useState<NoteCardData[]>([
+        {
+            cardKey: 1,
+            cardTitle: 'Chapter number/name',
+            cardDescription: 'Your highlights/notes on the chapter',
+        }
+    ]);
+
+    const [newCardsToAdd, setNewCardsToAdd] = useState<number>(1);
+    const [isSubModalOpen, setIsSubModalOpen] = useState<boolean>(false);
+
+    // Function to handle adding new cards
+    const addNewCards = (): void => {
+        if (noteCards.length + newCardsToAdd > 4) {
+            return setIsSubModalOpen(true); // Open modal if user exceeds limit
+        }
+
+        const newCards = Array.from({ length: newCardsToAdd }, (_, index) => ({
+            cardKey: noteCards.length + index + 1,
+            cardTitle: `Chapter number/name`,
+            cardDescription: `Your highlights/notes on the chapter`,
+        }));
+
+        setNoteCards((prevCards) => [...prevCards, ...newCards]);
+        window.scrollTo({top: document.body.scrollHeight, behavior: 'smooth'})
+    };
+
+    // Function to close the modal
+    const closeSubModal = (): void => {
+        setIsSubModalOpen(false); // Close modal
+    };
+
+    // Handle changes to how many cards to add
+    const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const value = parseInt(event.target.value);
+        setNewCardsToAdd(Math.max(1, Math.min(value, 4)));
+    };
+
+    const handleDeleteCard = (key: number): void => {
+        const updatedCards = noteCards.filter((card) => card.cardKey !== key);
+        const reorderedCards = updatedCards.map((card, index) => ({
+            ...card,
+            cardKey: index + 1
+        }));
+        setNoteCards(reorderedCards);
+    };
+
+    return (
+        <>
+            <GoBackToComponent />
+            <div className="createNotesComponent">
+                <NotesCreationButtons />
+                <NotesDescriptionAndTitleComponent />
+
+                {noteCards.map((card) => (
+                    <NoteCard
+                        key={card.cardKey}
+                        cardKey={card.cardKey}
+                        cardTitle={card.cardTitle}
+                        cardDescription={card.cardDescription}
+                        onDelete={handleDeleteCard}
+                    />
+                ))}
+
+                <div className="cardFooterContainer">
+                    <div className="addNewCardButton" onClick={addNewCards}>
+                        + Add card(s)
+                    </div>
+                    <input
+                        type="number"
+                        defaultValue={1}
+                        max={4}
+                        min={1}
+                        onChange={handleInputChange}
+                    />
+                </div>
+            </div>
+
+            {/* Render the modal if the user tries to add more than 4 cards */}
+            {noteCards.length === 4 && isSubModalOpen && (
+                <SubModalComponent onSubModalClose={closeSubModal} />
+            )}
+        </>
+    );
+}
