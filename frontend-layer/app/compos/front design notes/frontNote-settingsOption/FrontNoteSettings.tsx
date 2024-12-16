2114e444f1a06c1817fe72aea6be83c66502d816
jadTales <117829497+Jadtales@users.noreply.github.com>
2024-12-16 16:01:32 +0100

    - Fixed certain styling problems. - Better MoveNoteCardTo dialog performance. - Removed certain unnecessary files.

--- frontend-layer/app/compos/front design notes/frontNote-settingsOption/FrontNoteSettings.tsx
+++ frontend-layer/app/compos/front design notes/frontNote-settingsOption/FrontNoteSettings.tsx
@@ -1,17 +1,15 @@
 'use client';
 
-import React, {ReactElement, useEffect, useRef, useState} from "react";
+import React, {ReactElement, useEffect, useRef} from "react";
 import Image from "next/image";
 import './frontNoteSettingStyling.css';
 
 // imported icons
 import DownloadIcon from '@/public/icons/frontNoteSetting-icons/download-line.svg';
-import FolderTransferIcon from '@/public/icons/frontNoteSetting-icons/folder-transfer-line.svg';
 import EditNoteIcon from '@/public/icons/frontNoteSetting-icons/pencil-line.svg';
 import PinNoteIcon from '@/public/icons/frontNoteSetting-icons/pushpin-2-line.svg';
 import MoveToComponent
     from "@/app/compos/front design notes/frontNote-settingsOption/settingsComponents/MoveToComponent";
-import {router} from "next/client";
 import {useRouter} from "nextjs-toploader/app";
 
 interface FrontNoteSettingsProps {
@@ -28,13 +26,6 @@ export default function FrontNoteSettings({onClose, bookId, bookTitle}: FrontNot
 
     const prevent = (event: React.MouseEvent) => {
         event.stopPropagation();
-    };
-
-    const [isMoveToModalOpen, setIsMoveToModalOpen] = useState<boolean>(false);
-
-    // Toggle the modal
-    const openMoveToModal = () => {
-        setIsMoveToModalOpen((prevState) => !prevState);
     };
 
     // Detect click outside to close settings
@@ -56,19 +47,17 @@ export default function FrontNoteSettings({onClose, bookId, bookTitle}: FrontNot
         router.push(`/createnotes/${bookTitle.replaceAll(' ', '-')}-${bookId}`)
     }
 
+
     return (
         <div className="frontNoteSettingContainer" ref={settingsRef} onClick={prevent}>
             <ul>
                 <li><Image src={PinNoteIcon} width={20} alt="pinNote"/>Pin</li>
                 <li onClick={handleForwardToReviewPage}><Image src={EditNoteIcon} width={20} alt="editNote"/>Edit this Note</li>
-                <li onClick={openMoveToModal}><Image src={FolderTransferIcon} width={20} alt="moveNote"/>Move to</li>
+                <MoveToComponent/>
                 <li><Image src={DownloadIcon} width={20} alt="exportNote"/>Export</li>
                 <hr style={{margin: '5px 0'}}/>
                 <li>Delete</li>
             </ul>
-
-            {/* Conditional rendering of the modal */}
-            {isMoveToModalOpen && <MoveToComponent toggleModal={openMoveToModal}/>}
         </div>
     );
 }
